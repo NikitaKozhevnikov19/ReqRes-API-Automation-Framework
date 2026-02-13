2f01044aa806c4b7ce27fe25b2c80d5da7f2fbd0
Nikita Kozhevnikov <kozherka2@gmail.com>
2026-02-13 17:26:32 +0500

    renamed

--- src/test/java/models/UserAuthDto.java
+++ src/test/java/models/UserAuthDto.java
@@ -1,12 +1,14 @@
 package models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserRegistration {
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserAuthDto {
     private String email;
     private String password;
 }
